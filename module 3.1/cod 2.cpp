12ae267dfeb9075e37283561a86011f139a0258b
Kishanvekariya08 <140416333+Kishanvekariya08@users.noreply.github.com>
2023-08-22 14:37:15 +0530

    Add files via upload
    22 Aug.2023

---
+++ module 3.1/cod 2.cpp
@@ -0,0 +1,10 @@
+//Write a programme of My information
+#include<stdio.h>
+int main ()
+{
+	// local Section of the programme
+	printf("kishan vekariya\n"); 
+	printf("08 april 2003\n");
+	printf("21 year\n");
+	printf("block no.66, shreevilla satelight chowk morbi road, rajkot");		
+}
